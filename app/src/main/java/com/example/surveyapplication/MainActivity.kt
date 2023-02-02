2b6c3c60f98d23252ace73f174439da807dd901a
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-02-03 02:28:19 +0530

    role text added

--- app/src/main/java/com/example/surveyapplication/MainActivity.kt
+++ app/src/main/java/com/example/surveyapplication/MainActivity.kt
@@ -6,12 +6,14 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var nameInput: EditText
+    private lateinit var nameInput: EditText
+//    private lateinit var roleInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         nameInput = findViewById(R.id.name_input_box)
+//        roleInput = findViewById(R.id.input_role)
     }
 }
