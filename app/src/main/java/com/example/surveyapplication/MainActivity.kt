13feefe00f79ff86e652386909eb18f20e3cf480
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-02-03 02:18:36 +0530

    layout maintained properly!

--- app/src/main/java/com/example/surveyapplication/MainActivity.kt
+++ app/src/main/java/com/example/surveyapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.surveyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nameInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameInput = findViewById(R.id.name_input_box)
     }
 }
