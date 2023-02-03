927a2e8c8e74b3024727a5eb0bd98d6a66091bf7
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-02-03 14:37:48 +0530

    checkboxes clearing on clicking clear button

--- app/src/main/java/com/example/surveyapplication/EventsList.kt
+++ app/src/main/java/com/example/surveyapplication/EventsList.kt
@@ -2,10 +2,48 @@ package com.example.surveyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 
 class EventsList : AppCompatActivity() {
+
+    private lateinit var music: CheckBox
+    private lateinit var dance: CheckBox
+    private lateinit var play: CheckBox
+    private lateinit var fashion: CheckBox
+    private lateinit var food: CheckBox
+    private lateinit var submit: Button
+    private lateinit var clear: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events_list)
+
+        music = findViewById(R.id.music)
+        dance = findViewById(R.id.dance)
+        play = findViewById(R.id.play)
+        fashion = findViewById(R.id.fashion)
+        food = findViewById(R.id.food)
+        submit = findViewById(R.id.submitButton)
+        clear = findViewById(R.id.clearButton)
+
+        clear.setOnClickListener{
+            if(music.isChecked){
+                music.isChecked = false
+            }
+            if(dance.isChecked){
+                dance.isChecked = false
+            }
+            if(play.isChecked){
+                play.isChecked = false
+            }
+            if(fashion.isChecked){
+                fashion.isChecked = false
+            }
+            if(food.isChecked){
+                food.isChecked = false
+            }
+        }
+
     }
 }
